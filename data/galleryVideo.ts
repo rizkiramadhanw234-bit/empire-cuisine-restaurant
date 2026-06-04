8a4e23b840a79c471d09c01e8b7df13d809e5414
rizkiramadhanw234-bit <rizkiramadhanw234@gmail.com>
2026-06-04 15:56:32 +0700

    added new gallery videos

--- data/galleryVideo.ts
+++ data/galleryVideo.ts
@@ -20,4 +20,14 @@ export const GalleryVideo: GalleryItem[] = [
     category: "Videos",
     item: "https://res.cloudinary.com/dqp0y3avg/video/upload/v1780251041/41_Gallery_Page_ja4acs.mp4",
   },
+  {
+    id: 42,
+    category: "Videos",
+    item: "https://res.cloudinary.com/dqp0y3avg/video/upload/v1780563284/%E4%BC%A0%E8%AF%B4%E4%B8%AD%E7%9A%84%E4%BD%9B%E8%B7%B3%E5%A2%99_tv0poy.mp4",
+  },
+  {
+    id: 43,
+    category: "Videos",
+    item: "https://res.cloudinary.com/dqp0y3avg/video/upload/v1780563314/%E4%BC%A0%E8%AF%B4%E4%B8%AD%E7%9A%84%E4%BD%9B%E8%B7%B3%E5%A2%99_1_zuqwal.mp4",
+  },
 ];
