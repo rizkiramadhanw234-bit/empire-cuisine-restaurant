af0c618775a9ee271f7c4a501bcff0b83e0a2977
rizkiramadhanw234-bit <rizkiramadhanw234@gmail.com>
2026-06-09 10:59:20 +0700

    fixed navbar menu name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
     { id: 2, name: "About", href: "/about" },
     { id: 3, name: "Menu", href: "/menu" },
     { id: 4, name: "VIP Rooms", href: "/vipRooms" },
-    { id: 5, name: "Weddings & Events", href: "/weddingsAndEvents" },
+    { id: 5, name: "Banquets & Events", href: "/weddingsAndEvents" },
     { id: 6, name: " Gallery", href: "/gallery" },
     { id: 7, name: "Promotions", href: "/promotions" },
     { id: 8, name: "Membership & Benefits", href: "/membership" },
